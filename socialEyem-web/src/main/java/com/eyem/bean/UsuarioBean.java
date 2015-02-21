d303289884991ad138424560a11842b1f117b00e
Emilio <Emilio@epc>
2015-02-21 18:42:17 +0100

    init5

--- socialEyem-web/src/main/java/com/eyem/bean/UsuarioBean.java
+++ socialEyem-web/src/main/java/com/eyem/bean/UsuarioBean.java
@@ -14,15 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-
-
-
-
-
-
-
-
-
 @Component
 @Scope("session")
 public class UsuarioBean implements Serializable {

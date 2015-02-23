41e27f974a52ea5c172dfe70310f5b48c57679e4
Emilio <Emilio@epc>
2015-02-23 20:32:43 +0100

    post funcionando fecha

--- socialEyem-web/src/main/java/com/eyem/bean/PostBean.java
+++ socialEyem-web/src/main/java/com/eyem/bean/PostBean.java
@@ -46,11 +46,7 @@ public class PostBean implements Serializable {
         p.setContenido(contenido);
         p.setImagen(imagen);
         p.setIdPost(System.currentTimeMillis());
-        
-//        long yourmilliseconds = 1119193190;
-//        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
-//        Date resultdate = new Date(yourmilliseconds);
-//        System.out.println(sdf.format(resultdate));
+     
         
         postService.crearPost(p);
         listaPost = postService.buscarTodos();
